477e8b4421b7a3070438e0d4c9419881905e92f8
dinhduong34 <kutung34@gmail.com>
2018-03-17 12:18:15 +0700

    create film-save film database

---
+++ API/controller/todosController.js
@@ -0,0 +1,142 @@
+// var Todos = require("../model/todosFilm");
+
+// function getTodos(res) {
+//     Todos.find(function (err, todos) {
+
+//         if (err) {
+//             res.status(500).json(err);
+//         } else {
+//             res.json(todos);
+//         }
+//     });
+// }
+
+// module.exports = function (app) {
+//     //get all todos
+//     app.get("/api/todos", function (req, res) {
+//         getTodos(res)
+//     });
+
+//     // / api/todo/123456
+//     app.get("/api/todo/:id", function (req, res) {
+//         Todos.findById({ _id: req.params.id }, function (err, todo) {
+//             if (err) {
+//                 throw err;
+//             } else {
+//                 res.json(todo);
+//             }
+//         })
+//     });
+
+
+//     //create a todo
+//     app.post("/api/todo", function (req, res) {
+//         var todo = {
+//             text: req.body.text,
+//             isDone: req.body.IsDone
+//         };
+
+//         Todos.create(todo, function (err, todo) {
+//             if (err) {
+//                 throw err;
+//             } else {
+//                 getTodos(res);
+//             }
+//         });
+//     });
+//     //update a todo
+
+//     app.put("/api/todo", function (req, res) {
+//         if (!req.body.id) {
+//             return res.status(500).send("ID is required")
+//         } else {
+//             Todos.update({
+//                 __id: req.body.id
+//             }, {
+//                     text: req.body.text.isDone,
+//                     isDone: req.body.IsDone
+//                 }, function (err, todo) {
+//                     if (err) {
+//                         return res.status(500).json(err);
+//                     } else {
+//                         getTodos(res);
+//                     }
+//                 })
+//         }
+//     });
+//     //delete todo
+//     app.delete("/api/todo/:id", function (req, res) {
+//         Todos.remove({
+//             __id: req.params.id
+//         }, function (err, todoo) {
+//             if (err) {
+//                 return res.status(500).json(err);
+//             } else {
+//                 getTodos(res);
+//             }
+//         });
+//     });
+// }
+
+
+
+
+// var Todos = require("../model/todosFilm");
+
+// function getTodos(res) {
+//     Todos.find(function (err, todos) {
+
+//         if (err) {
+//             res.status(500).json(err);
+//         } else {
+//             res.json(todos);
+//         }
+//     });
+// }
+
+
+
+
+
+
+
+
+var Films = require("../model/todosFilm");
+
+function getFilm(res) {
+    Films.find(function (err, filmms) {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.json(filmms);
+        }
+    });
+}
+
+
+module.exports = function (app) {
+    //get all todos
+    // app.get("/api/CreateFilm", function (req, res) {
+
+    // });
+    app.post("/api/CreateFilm", function (req, res) {
+        var Film = {
+            NameFilm: req.body.NameFilm,
+            StyleFilm: req.body.StyleFilm,
+            Comment: req.body.Comment
+        };
+        Films.create(Film, function (err, Film) {
+            if (err) {
+                throw err;
+            } else {
+                getFilm(res);
+            }
+        });
+    });
+}
+
+// module.exports = function (app) {
+//     app.get("API/CreateFilm", function (req, res) {
+//         getFilm(res);
+//     })
+// }
---
+++ routes/inputFilm.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('tao-phim', { title: 'Express' });
+});
+
+//Hiện thông tin phim lên console khi ta đăng phim
+router.post('/', function(req,res,next){
+  console.log("Ten Phim: " + req.body.NameFilm);
+  console.log("The Loai phim: " + req.body.StyleFilm)
+  console.log("Nam phat hanh phim: " +req.body.month + req.body.year);
+  console.log("Binh luan: " + req.body.Comment);
+
+
+})
+
+module.exports = router;
